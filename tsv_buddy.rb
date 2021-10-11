a48a125b0f4d47cf8da859fef300f5445a6c3eb9
Karen Chiu <karenchiu@KarendeMacBook-Air.local>
2021-10-11 22:56:56 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -4,11 +4,31 @@ module TsvBuddy
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
 
+    rows = tsv.split("\n").map { |line| line.split("\t") }
+    headers = rows.first
+    data = rows[1..-1]
+    key_values = data.map do |row|
+      row.map.with_index { |cell, i| [headers[i], cell] }.to_h
+    end
+    @data = key_values
+
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+
+    keys_array = @data[0].keys
+    keys_line = ''
+    keys_array.each { |key| keys_line.concat("#{key}\t") }
+    file = "#{keys_line}\n"
+    
+    @data.each do |record|
+      record.each_value { |value| file << "#{value}\t" }
+      file << "\n"
+    end
+
+    file
 
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,30 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in YAML format
+  require 'yaml'
+
+  def take_yaml(yaml)
+
+    survey = YAML.safe_load(yaml)
+    keys_array = survey[0].keys
+    value_array = []
+    survey.each do |record|
+      record.each_value { |value| value_array << value }
+    end
+
+    record = {}
+    keys_array.each_index { |index| record[keys_array[index]] = value_array[index].chomp }
+    survey.push(record)
+    survey.pop
+    @data = survey
+
+  end
+  
+  # to_yaml: converts @data into yaml string
+  # returns: String in YAML format
+  def to_yaml
+    @data.to_yaml
+  end
+end
+  
